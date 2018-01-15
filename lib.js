a11cecff68ef153e5e635f074905754877363df8
Frederic Gingras <frederic@gingras.cc>
2018-01-14 23:29:45 -0500

    work on DB and Repository classes

--- lib.js
+++ lib.js
@@ -3,11 +3,18 @@ const url = require("url");
 const path = require("path");
 const http = require("http");
 const { promisify } = require("util");
-const { hash } = require("./utils");
+const {
+  hash,
+  snakeCase,
+  ulid,
+  ulidToUuid,
+} = require("./utils");
 
 // mailer / http / view / job / data / storage / support
 
 const append = a => b => b.concat([a]);
+
+const contains = item => list => list.indexOf(item) !== -1;
 
 const MIME_TYPES = {
   "js": "text/javascript",
@@ -95,6 +102,261 @@ class FileSystem {
     return contents;
   }
 }
+
+// data
+
+class DB {
+  constructor(config) {
+    this.config = config;
+    this.databaseUrl = config.get("databaseUrl");
+  }
+
+  query(sql, values) {
+    if (!config.get("production")) {
+      // TODO use logger
+      console.log("executing sql '" + sql + "'");
+    }
+    return new Promise((resolve, reject) => {
+      pg.connect(this.databaseUrl, (err, client, done) => {
+        if (err) return reject(err);
+        client.query(sql, values, (err, result) => {
+          done();
+          if (err) return reject(err);
+          resolve(result);
+        });
+      });
+    });
+  }
+
+  setWheres(query, where = {}) {
+    if (Object.keys(where).length === 0) return query;
+    query.sql += " where";
+    query.values = query.values || [];
+    for (let field of Object.keys(where)) {
+      const value = where[field];
+      if (field.slice(0, 3) === "sql") {
+        query.sql += ` ${value[0]}`;
+        query.values = query.values.concat(value.slice(1));
+      } else {
+        query.sql += ` ${field} = ?`;
+        query.values.push(value);
+      }
+    }
+    return query;
+  }
+
+  async select(table, options = {}) {
+    const query = { sql: `SELECT * FROM ${table}`, values: [] };
+    this.setWheres(query, options.where);
+    if (options.offset) {
+      query.sql += " OFFSET ?";
+      query.values.push(options.offset);
+    }
+    if (options.limit) {
+      query.sql += " LIMIT ?";
+      query.values.push(options.limit);
+    }
+    if (options.sort) {
+      if (!Array.isArray(options.sort) || options.sort.length === 0) {
+        throw new Error("db.select: `sort` is not an array");
+      }
+      let sortClauses = [];
+      for (let sort in options.sort) {
+        let direction = (sort[0] === "-") ? "DESC" : "ASC";
+        if (sort[0] === "-") sort = sort.slice(1);
+        sortClauses.push(`? ${direction}`);
+        query.values.push(sort);
+      }
+      query.sql += " ORDER BY " + sortClauses.join(", ");
+    }
+
+    const result = await this.query(query.sql, query.values);
+    return result.rows;
+  }
+
+  async count(table, options) {
+    const sql = `SELECT COUNT(*) as count FROM ${table}`;
+    const query = { sql, values: [] };
+    this.setWheres(query, options.where);
+    const result = await this.query(query.sql, query.values);
+    return parseInt(result.rows["0"].count, 10);
+  }
+
+  selectOne(table, where) {
+    return this.select(table, { where, limit: 1 }).then(xs => xs[0]);
+  }
+
+  async insert(table, obj) {
+    const fields = Object.keys(givenObj);
+
+    const sql = `INSERT INTO ${table}`;
+    const query = { sql, values: [] };
+    query.sql += ` (${fields.join(", ")})`;
+    query.sql += ` VALUES (${fields.map(() => "?").join(", ")})`;
+    fields.forEach(field => query.values.push(obj[field]));
+    query.sql += " RETURNING *";
+
+    const result = await this.query(query.sql, query.values);
+    return result.rows["0"];
+  }
+
+  async update(table, where, obj) {
+    const fields = Object.keys(obj);
+
+    const sql = `UPDATE ${table} SET `;
+    const query = { sql, values: [] };
+    const setStatements = [];
+    for (let field of fields) {
+      if (obj[field] !== undefined && obj[field] !== null) {
+        setStatements.push(` ${field} = ?`);
+        query.values.push(obj[field]);
+      }
+    }
+    query.sql += setStatements.join(", ");
+    this.setWheres(query, where);
+    query.sql += " RETURNING *";
+
+    const result = await this.query(query.sql, query.values);
+    return result.rows["0"];
+  }
+
+  delete(table, where) {
+    const query = { sql: `DELETE FROM ${table}`, values: [] };
+    this.setWheres(query, where);
+    return this.query(query.sql, query.values);
+  }
+
+  async save(table, obj, primaryKey = "id") {
+    if (obj[primaryKey]) {
+      const pk = ulidToUuid(obj[primaryKey]);
+      const where = { [primaryKey]: pk };
+      delete obj[primaryKey];
+      obj = await this.update(table, where, obj);
+      obj[primaryKey] = pk;
+      return obj;
+    }
+    obj[primaryKey] = ulidToUuid(ulid());
+    return await this.insert(table, obj);
+  }
+}
+
+class Repository {
+  constructor(db) {
+    this.db = db;
+
+    this.primaryKey = "id";
+    this.table = null;
+    this.entityClass = null;
+  }
+
+  async find(where, limit, offset) {
+    if (!this.entityClass) throw new Error("Entity is missing entityClass");
+    const options = {};
+    if (where) options.where = where;
+    if (limit) options.limit = limit;
+    if (offset) options.offset = offset;
+
+    const rows = await this.db.select(this.table, options);
+
+    return rows.map(this.entityClass.fromDatabase.bind(Entity));
+  }
+
+  findOne(where) {
+    return this.find(where, 1).then(xs => xs[0]);
+  }
+
+  findById(pk) {
+    return this.findOne({ [this.primaryKey]: pk });
+  }
+
+  save(entity) {
+    const obj = entity.toDatabase();
+    return this.entityClass.fromDatabase(
+      this.db.save(table, obj, this.primaryKey)
+    );
+  }
+}
+
+class Entity {
+  constructor(params, skipDefaults, givenDefaults = {}) {
+    const defaults = skipDefaults ? {} : givenDefaults;
+    const obj = Object.assign({}, defaults, params);
+
+    for (let key of Object.keys(obj)) {
+      const value = obj[key];
+      this[key] = (typeof value === "function") ? value() : value;
+    }
+  }
+
+  toJson(includePrivate = false) {
+    const fieldNames = Object.keys(this.fields || {});
+    const privateFields = this.privateFields || [];
+    const obj = {};
+
+    for (let key of fieldNames) {
+      if (!includePrivate && contains(key, privateFields)) continue;
+      obj[key] = this[key];
+    }
+
+    return obj;
+  }
+
+  toObject() {
+    return this.toJson(true);
+  }
+
+  toDatabase() {
+    const fields = this.fields || {};
+    const obj = {};
+    for (let key of Object.keys(fields)) {
+      const field = fields[key];
+      obj[snakeCase(key)] = field.toDatabase(this[key]);
+    }
+    return obj;
+  }
+
+  fromDatabase(obj) {
+    const fields = this.fields || {};
+    for (let key of Object.keys(fields)) {
+      const field = fields[key];
+      this[key] = field.fromDatabase(obj[snakeCase(key)]);
+    }
+    return this;
+  }
+
+  static fromDatabase(obj) {
+    return new this().fromDatabase(obj);
+  }
+
+  static newDate() {
+    return new Date();
+  }
+}
+
+Entity.table = null;
+
+const EntityFields = {
+  Text: {
+    toDatabase: x => x,
+    fromDatabase: x => x,
+  },
+  Number: {
+    toDatabase: x => x,
+    fromDatabase: x => x,
+  },
+  Date: {
+    toDatabase: x => x.toISOString(),
+    fromDatabase: x => new Date(x),
+  },
+  Ulid: {
+    toDatabase: x => ulidToUuid(x),
+    fromDatabase: x => uuidToUlid(x),
+  },
+  Object: {
+    toDatabase: x => JSON.stringify(x),
+    fromDatabase: x => JSON.parse(x),
+  },
+};
 
 // view
 
--- utils.js
+++ utils.js
@@ -15,10 +15,20 @@ function noCase(str, replacement = " ") {
     .toLowerCase();
 }
 
-const snakeCase = value => noCase(value, "_");
 const paramCase = value => noCase(value, "-");
+const snakeCase = value => noCase(value, "_");
 const camelCase = value =>
   noCase(value).replace(/ (.)/g, (_, v) => v.toUpperCase());
+
+const caseKeys = (caseFn) => (obj) => {
+  const newObj = {};
+  for (let key of Object.keys(obj)) {
+    newObj[caseFn(key)] = obj[key];
+  }
+  return newObj;
+};
+const snakeCaseKeys = caseKeys(snakeCase);
+const camelCaseKeys = caseKeys(camelCase);
 
 function hash(str) {
   let hash = 5381;
@@ -31,8 +41,47 @@ function hash(str) {
   return hash >>> 0;
 }
 
+const ULID_CHARS = "0123456789ABCDEFGHJKMNPQRSTVWXYZ";
+const ULID_CHARS_LENGTH = ULID_CHARS.length;
+
+function ulid() {
+  let str = "";
+  let now = Date.now();
+  let mod;
+  for (let i = 10; i > 0; i--) {
+    mod = now % ULID_CHARS_LENGTH;
+    str = ULID_CHARS.charAt(mod) + str;
+    now = (now - mod) / ULID_CHARS_LENGTH;
+  }
+  for (let i = 16; i > 0; i--) {
+    let rand = Math.floor(Math.random() * ULID_CHARS_LENGTH);
+    if (rand >= ULID_CHARS_LENGTH) rand = ULID_CHARS_LENGTH-1;
+    str = str + ULID_CHARS.charAt(rand);
+  }
+  return str;
+}
+
+function ulidToUuid(id) {
+  return [
+    id.slice(0, 8),
+    id.slice(8, 12),
+    id.slice(12, 16),
+    id.slice(16, 20),
+    id.slice(20, 26) + "000000",
+  ].join("-");
+}
+
+function uuidToUlid(id) {
+  return id.replace("-", "").slice(0, 26);
+}
+
 exports.noCase = noCase;
-exports.snakeCase = snakeCase;
 exports.paramCase = paramCase;
+exports.snakeCase = snakeCase;
 exports.camelCase = camelCase;
+exports.snakeCaseKeys = snakeCaseKeys;
+exports.camelCaseKeys = camelCaseKeys;
 exports.hash = hash;
+exports.ulid = ulid;
+exports.ulidToUuid = ulidToUuid;
+exports.uuidToUlid = uuidToUlid;
