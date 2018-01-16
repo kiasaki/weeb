b08b4a3d914ee101d0483be3930c9f80cfc28a5e
Frederic Gingras <frederic@gingras.cc>
2018-01-16 06:53:34 -0500

    Split up lib.js and get back to working state

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const { Application } = require("./lib.js");
+const { Project } = require("./lib");
 
-const app = new Application();
+const app = new Project();
 app.addApplication("app", __dirname + "/app");
 app.router.get("/", (req, res) => res.template(200, "home", { name: "Joe" }));
 app.start();
